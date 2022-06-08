30c96bd9c43b728e7626b908db6710cab1c4dcff
makhrosenkov <106873930+makhrosenkov@users.noreply.github.com>
2022-06-08 18:29:20 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -5,3 +5,17 @@
 # Для всех остальных случаев не выводить ничего.
 
 # TODO: your code here
+
+
+print ('Введите целое число: ')
+integer = int(input())
+foo = 3
+bar = 5
+
+if integer % foo == 0 and integer % bar == 0:
+    print ('foobar')
+else:
+    if integer % foo == 0:
+        print ('foo')
+    if integer % bar == 0:
+        print ('bar')
