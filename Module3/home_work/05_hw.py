de899437da7fb1c0d2bf6a3b5c5734eee2c1025c
makhrosenkov <106873930+makhrosenkov@users.noreply.github.com>
2022-06-15 21:51:49 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,16 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр", "Святослав", "Святослав"]
+name_length = []
+print("Динные имена: ", names)
+count = 0
+the_longest = 0
+for b in names:
+    name_length = len(b)
+    if name_length > count:
+        the_longest = name_length
+        count = the_longest
+        name = b
+print("Но самое длинное:", name)
