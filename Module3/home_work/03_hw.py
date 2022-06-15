2b81e78acb16a2bf01979d908f89b9834019d99a
makhrosenkov <106873930+makhrosenkov@users.noreply.github.com>
2022-06-15 21:17:50 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,28 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+
+import random
+numbers = []
+
+print ('Введите количество чисел в списке ')
+n = int(input ())
+count = 1
+
+
+while count <= n:
+    numbers.append (random.randint(-100, 100))
+    count +=1
+print ("Ваш список: ", numbers)
+count = 1
+summ = 0
+positive_even_numbers=[]
+for b in numbers:
+#    print (b, end=" ")
+    if b > 0 and b % 2 == 0:
+        positive_even_numbers.append(b)
+        #print ("Положительные чётные числа: ", b)
+        summ = summ + b
+print ("Положительные чётные числа: ",
+       positive_even_numbers)
+print ("Cумма этих чисел: ", summ)
