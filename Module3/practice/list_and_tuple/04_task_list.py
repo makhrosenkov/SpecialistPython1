fdd66c5729df6167c6cfecf7874a9d71efe8aec7
makhrosenkov <106873930+makhrosenkov@users.noreply.github.com>
2022-06-18 12:39:40 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,18 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+
+import random
+
+print('Введите количество чисел в списке')
+n = int(input())
+numbers = []
+summ = 0
+
+for _ in range(n):
+    numbers.append(random.randint(-10, 10))
+for number in numbers:
+    if number > 0:
+        summ = summ + number
+print(numbers)
+print(summ)
