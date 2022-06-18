58c7d76699675e13bed296062f179079c62accd8
makhrosenkov <106873930+makhrosenkov@users.noreply.github.com>
2022-06-18 14:48:40 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,30 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+
+first_number = int(input())     # Первое число
+second_number = int(input())    # Второе число
+numbers = []
+multiples_of_three = []
+
+summ = 0
+if first_number <= second_number:
+
+     while first_number <= second_number:
+          numbers.append(first_number)
+          if first_number % 3 == 0:
+               multiples_of_three.append(first_number)
+
+          first_number = first_number + 1
+
+     #i += i
+elif second_number <= first_number:
+     while second_number <= first_number:
+          numbers.append(second_number)
+          second_number=second_number+1
+          if second_number % 3 == 0:
+               multiples_of_three.append(second_number)
+          #print (numbers)
+
+print (numbers)
+print("кратные трём: ", multiples_of_three)
