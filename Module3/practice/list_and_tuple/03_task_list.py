39ee7ad9c18a6ba68dab88e06e101421cfa06582
makhrosenkov <106873930+makhrosenkov@users.noreply.github.com>
2022-06-18 12:32:22 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,13 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+
+numbers1 = []
+summ = 0
+number = 0
+for _ in range(n):
+    numbers1.append(random.randint(-100, 100))
+for number in numbers1:
+    summ = summ + number
+print(numbers1)
+print(summ)
