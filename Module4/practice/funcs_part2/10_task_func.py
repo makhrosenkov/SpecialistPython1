ae7f127c2ec1009b38a7ad4b228fae44e502d9f5
makhrosenkov <106873930+makhrosenkov@users.noreply.github.com>
2022-06-25 11:25:43 +0300

    Update 10_task_func.py

--- Module4/practice/funcs_part2/10_task_func.py
+++ Module4/practice/funcs_part2/10_task_func.py
@@ -9,3 +9,20 @@ def average(*args):
 print(average(3, 4, 8))
 print(average(1, 4, 5, -3, 8, 4))
 print(average(-10, 12, 6.3, -5.5, 7, 0.2))
+
+
+
+def average(*args):
+    summ = 0
+    i = 0
+    for el in args:
+        summ += el
+        i = i + 1
+
+    summ == summ / i
+    return summ
+
+
+print(average(3, 4, 8))
+print(average(1, 4, 5, -3, 8, 4))
+print(average(-10, 12, 6.3, -5.5, 7, 0.2))
